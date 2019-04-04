2a6be203ed0e0da0fda466ed73c2c8b20b0a337d
akhilesh <akhileshseechurn@gmail.com>
2019-04-05 01:21:01 +0400

    [c-language] [Hello World], [Using the preprocessor]

---
+++ Hello World/main.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int main() {
+    puts("Hello, World!\n");
+    printf("Hello, World!");
+    return 0;
+}
---
+++ Using the preprocessor/main.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+#define MESSAGE "Hello, World\n"
+#define ZERO 0
+
+int main() {
+    printf(MESSAGE);
+    return ZERO;
+}
