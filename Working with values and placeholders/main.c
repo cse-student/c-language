72970a9e6e710a5664de4cfe0767551e755aeb03
akhilesh <akhileshseechurn@gmail.com>
2019-04-19 00:52:15 +0400

    [c-language] Using placeholders with c-language in the prinf function

---
+++ Working with values and placeholders/main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+    printf("Printing an int: %d\n", 10);
+    printf("Printing a character: %c\n", 'c');
+    printf("Printing a floating point number: %f\n", 3.141592);
+    printf("Printing a floating point number with specific quantity of digits after the decimal: %0.2f\n", 3.141592);
+    printf("Printing a string: %s\n", "Hello World");
+    printf("Printing a percentage: 10%%");
+
+    return 0;
+}
