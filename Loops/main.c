d3c56b2eb170140317d1e39510b67cd9f2b71679
akhilesh <akhileshseechurn@gmail.com>
2019-04-22 00:57:24 +0400

    [c-language][Loops] While loops

---
+++ Loops/main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+    //region While-do loop
+    int i = 0;
+    while(i < 10){
+        printf("i =  %d\n", i);
+        i++;
+    }
+    //endregion
+    return 0;
+}
