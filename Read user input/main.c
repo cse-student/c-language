5238a23c201869f18cf6d1fd061e9c73a3b078a0
akhilesh <akhileshseechurn@gmail.com>
2019-04-21 21:03:31 +0400

    [c-language] Read user input through console

--- Read user input/main.c
+++ Read user input/main.c
@@ -4,7 +4,7 @@ int main() {
 
     //region Reading a character
     char c;
-    printf("Enter a character: ");
+    printf("Enter a character: \n");
     c = getchar();
     printf("character = %c", c);
     putchar('\n');
@@ -15,7 +15,7 @@ int main() {
 
     //region scanf
     //region Reading an character using scanf
-    printf("Enter another character: ");
+    printf("Enter another character: \n");
     scanf("%c", &c);
     printf("character = %c", c);
     putchar('\n');
@@ -26,7 +26,7 @@ int main() {
 
     //region Reading an integer using scanf
     int number;
-    printf("Enter a number: ");
+    printf("Enter a number: \n");
     scanf("%d", &number);
     printf("number = %d", number);
     putchar('\n');
@@ -35,16 +35,32 @@ int main() {
     //flush buffer
     fseek(stdin,0,SEEK_END);
 
-    //region Reading a floating-point number
+    //region Reading a floating-point number using scanf
     float fnumber;
-    printf("Enter a floating point number: ");
+    printf("Enter a floating point number: \n");
     scanf("%f", &fnumber);
-    printf("floating point number = %f", fnumber);
+    printf("floating point number = %f\n", fnumber);
 
     //flush buffer
     fseek(stdin,0,SEEK_END);
     //endregion
 
+    //region Reading words number using scanf
+    char word[256];
+    printf("Enter a word: \n");
+    scanf("%s", word);
+    printf("word = %s\n", word);
+    //endregion
+
+    //flush buffer
+    fseek(stdin,0,SEEK_END);
+    //endregion
+
+    //region Reading words with whitespace number using scanf
+    char sentence[256];
+    printf("Enter a sentence with whitespace: \n");
+    scanf("%s[^x]s", sentence);
+    printf("sentence = %s\n", sentence);
     //endregion
     return 0;
 }
