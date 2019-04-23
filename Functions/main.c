1564a376744463b4f69ac1d3f5fdfe26b859d4f1
akhilesh <akhileshseechurn@gmail.com>
2019-04-23 23:43:18 +0400

    [c-language][Functions] Functions with return values

--- Functions/main.c
+++ Functions/main.c
@@ -1,17 +1,32 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 
 //region Prototype
+
+//region void functions
 void add();
 void subtract();
 void multiply();
 void divide();
 //endregion
 
+//region with return values
+int randomNumGenerator();
+//endregion
+
+//endregion
+
+
 int main() {
     add();
     subtract();
     multiply();
     divide();
+
+    int randInt = randomNumGenerator();
+    printf("Random number = %d\n", randInt);
+
     return 0;
 }
 
@@ -29,4 +44,10 @@ void multiply(){
 
 void divide(){
     printf("10 / 2 = %d\n", 10/2);
+}
+
+int randomNumGenerator(){
+    srand(time(0));
+    int result = rand() % 50;
+    return result;
 }
