b4eb0edd73a3f33ac0bcb65b5ea44fce37ea9c7a
akhilesh <akhileshseechurn@gmail.com>
2019-04-29 00:07:46 +0400

    [c-language][Pointers and memory allocation] Data Memory Address

--- Pointers and memory allocation/main.c
+++ Pointers and memory allocation/main.c
@@ -4,6 +4,7 @@
 
 int main() {
     dataStructureSize();
+    memoryAddress();
     return 0;
 }
 
@@ -13,9 +14,29 @@ void dataStructureSize(){
     char string[64];
 
     //region Print size
-    printf("int size: %d\n", sizeof(a));
-    printf("character size: %d\n", sizeof(c));
-    printf("string size: %d\n", sizeof(string));
-    printf("laptop structure size: %d\n", sizeof(struct Laptop));
+    printf("##### Data structures sizes #####\n");
+    printf("int size: %lu\n", sizeof(a));
+    printf("character size: %lu\n", sizeof(c));
+    printf("string size: %lu\n", sizeof(string));
+    printf("laptop structure size: %lu\n", sizeof(struct Laptop));
+    //endregion
+}
+
+void memoryAddress(){
+    int a;
+    char c;
+    char string[64];
+    struct Laptop laptop = {
+            "HP",
+            2500.00,
+            16,
+            "Intel Core i7-9600K"
+    };
+    //region Print memory address
+    printf("##### Memory addresses #####\n");
+    printf("int memory address: %p\n", &a);
+    printf("character memory address: %p\n", &c);
+    printf("string memory address: %p\n", &string);
+    printf("laptop structure memory address: %p\n", &laptop);
     //endregion
 }
--- Pointers and memory allocation/prototypes.c
+++ Pointers and memory allocation/prototypes.c
@@ -1,1 +1,2 @@
 void dataStructureSize();
+void memoryAddress();
